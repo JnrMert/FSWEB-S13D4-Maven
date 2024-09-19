fca42f818924d37c2663e318c4cb4ee49298b516
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-19 19:38:46 +0300

    Done.

--- src/main/java/org/example/Weapon.java
+++ src/main/java/org/example/Weapon.java
@@ -1,10 +1,22 @@
 package org.example;
 
-public class Weapon {
-    enum weapon{
-        Sword,
-        Pistol
-    }
+    public enum Weapon {
+        SWORD(50, 1.2),
+        PISTOL(30, 3.0);
 
-    //Sword.getDamage
-}
+        private final int damage;
+        private final double attackSpeed;
+
+        Weapon(int damage, double attackSpeed) {
+            this.damage = damage;
+            this.attackSpeed = attackSpeed;
+        }
+
+        public int getDamage() {
+            return damage;
+        }
+
+        public double getAttackSpeed() {
+            return attackSpeed;
+        }
+    }
